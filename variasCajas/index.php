35c79ded9e0e7359a0470640541cd4d6f5260299
Fernando <faranzabe@cifpvirgendegracia.com>
2020-10-26 18:38:17 +0100

    Commit Ejemplo_VariasCajas

---
+++ variasCajas/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <form name="formulario" action="valida.php" method="POST">
+            <input type="text" name="caja[]" value="">
+            <input type="text" name="caja[]" value="">
+            <input type="text" name="caja[]" value="">
+            <input type="text" name="caja[]" value="">
+            <input type="submit" name="aceptar" value="Aceptar">
+        </form>
+    </body>
+</html>
---
+++ variasCajas/valida.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        $vec = $_REQUEST['caja'];
+        print_r($vec);echo '<br>';
+        foreach ($vec as $dato) {
+            echo $dato.'<br>';
+        }
+        ?>
+    </body>
+</html>
